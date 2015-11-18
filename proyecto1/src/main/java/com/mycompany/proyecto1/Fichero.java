42a9ac9af3f027e322e0217a71ad92c9fe3fa3ba
Jose Miguel Torres <jotorli@hotmail.com>
2015-11-18 13:14:01 +0100

    creacion clase fichero

---
+++ proyecto1/src/main/java/com/mycompany/proyecto1/Fichero.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proyecto1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author formacion
+ */
+public class Fichero {
+    private String arch;
+    public Fichero(String arch){
+    this.arch=arch;}
+    
+    public void leerFichero(){
+    File archivo = null;
+      FileReader fr = null;
+      BufferedReader br = null;
+ 
+      try {
+       
+         fr = new FileReader (arch);
+         br = new BufferedReader(fr);
+ 
+         // Lectura del fichero
+         String linea;
+         while((linea=br.readLine())!=null)
+            System.out.println(linea);
+      }
+      catch(Exception e){
+         e.printStackTrace();
+      }finally{
+         // En el finally cerramos el fichero, para asegurarnos
+         // que se cierra tanto si todo va bien como si salta 
+         // una excepcion.
+         try{                    
+            if( null != fr ){   
+               fr.close();     
+            }                  
+         }catch (Exception e2){ 
+            e2.printStackTrace();
+         }
+      }
+   }
+    public void escribirFichero(){
+    FileWriter fichero = null;
+        PrintWriter pw = null;
+        try
+        {
+            fichero = new FileWriter("textoprueba.txt");
+            pw = new PrintWriter(fichero);
+ 
+            for (int i = 0; i < 10; i++)
+                pw.println("Linea " + i);
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+           try {
+           // Nuevamente aprovechamos el finally para 
+           // asegurarnos que se cierra el fichero.
+           if (null != fichero)
+              fichero.close();
+           } catch (Exception e2) {
+              e2.printStackTrace();
+           }
+        }
+    }
+    
+    }
+    
+
