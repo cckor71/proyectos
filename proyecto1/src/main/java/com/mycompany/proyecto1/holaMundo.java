b94e6036ae0ed583eb42607c526ef0ebd9421ca4
Carlos <mail>
2015-11-18 13:28:05 +0100

    Proyecto conjunto

--- proyecto1/src/main/java/com/mycompany/proyecto1/holaMundo.java
+++ proyecto1/src/main/java/com/mycompany/proyecto1/holaMundo.java
@@ -10,5 +10,10 @@ package com.mycompany.proyecto1;
  * @author formacion
  */
 public class holaMundo {
-    
+    public static void main (String[] args){
+        Fichero f = new Fichero("nombre.txt");
+        f.escribirFichero();
+        f.leerFichero();
+        
+    }
 }
